9c3bd0c39cfac2b8e6a66463873e6365a43f4b27
Yasin Siraj Shamrat <yshamrat@gmail.com>
2024-10-23 11:36:50 +0600

    laucher icon & splash added

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MyAppState extends State<MyApp> {
       setState(() {
         _authenticated = val;
       });
-      FlutterNativeSplash.remove();
+      // FlutterNativeSplash.remove();
     });
   }
 
